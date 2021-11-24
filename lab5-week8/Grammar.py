0ca59a27e09bf443e34b9533ae1766d06a8d1f40
Pamfil Catalin <catalin.pamfil@gmail.com>
2021-11-24 08:25:58 +0200

    something

--- lab5-week8/Grammar.py
+++ lab5-week8/Grammar.py
@@ -19,12 +19,14 @@ class Grammar:
             for line in lines:
                 line = line.strip()
                 if lineNumber == 0:
-                    self.nonterminals = line.split(",")
+                    self.nonterminals = line.split(" ")
                 elif lineNumber == 1:
-                    self.terminals = line.split(",")
+                    self.terminals = line.split(" ")
                 elif lineNumber == 2:
                     self.startingSymbol = line
                 else:
+                    if line.strip() == "":
+                        continue
                     # do we need to save spaces?
                     # A ~ a 1 B| c
                     parts = line.split("~")
@@ -32,7 +34,7 @@ class Grammar:
                     if parts[0] not in self.productions:
                         self.productions[parts[0]] = []
                     for individualProduction in parts[1].split("|"):
-                        self.productions[parts[0]].append(individualProduction.split(" "))
+                        self.productions[parts[0]].append(individualProduction.strip().split(" "))
                 lineNumber += 1
 
     
@@ -42,15 +44,17 @@ class Grammar:
             return self.productions[givenNonterminal]
 
     def CFGcheck(self):
-        for leftSide in self.productions.keys:
+        for leftSide in self.productions.keys():
             if leftSide not in self.nonterminals:
                 return False
+        return True
 
 a = Grammar()
 a.readFile("g1.txt")
-print(a.nonterminals)
-print(a.terminals)
+#print(a.nonterminals)
+#print(a.terminals)
 print(a.productions)
+#print(a.CFGcheck())
 
 
     
--- lab5-week8/Parser.py
+++ lab5-week8/Parser.py
@@ -4,12 +4,35 @@ from Grammar import Grammar
 
 
 class Parser:
-    grammar = Grammar()
+    def __init__(self) -> None:
+        self.grammar = Grammar()
+        #[[[],[]],[[]]]
+        self.allSets = []
+        #[[],[],[]]
+        self.forCheckingAllSets = []
+    
+    #[["S'",["~","S"]],[...]]
+    #["~","S"].index("~")
+    def closure(self,set):
+        #["S'",["~","S"]]
+        newSet = []
+        elementArray = set[1]
+        pointIndex = elementArray.index("~")
+        if pointIndex + 1 == len(elementArray):
+            newSet.push(set)
+        else
+        #if newSet has any new elements
 
-    def closure(self):
-        pass
-    def goto(self):
+
+    def goto(self,set,element):
+        for individualSet in set:
+            individualSetArray = individualSet[1]
+            pointIndex = individualSetArray.index("~")
+            if pointIndex<len(individualSetArray) and individualSetArray[pointIndex+1] == element:
+                pass
         pass
     def CanonicalCollection(self):
         pass
         #work with ~ in place of .
+#print(["~","S"].index("~"))
+
