b9d8ffb1cb35a759c49ce3937aefb65b312167c3
Pamfil Catalin <catalin.pamfil@gmail.com>
2021-11-24 07:39:21 +0200

    started lab6-week9

---
+++ lab5-week8/Parser.py
@@ -0,0 +1,15 @@
+
+
+from Grammar import Grammar
+
+
+class Parser:
+    grammar = Grammar()
+
+    def closure(self):
+        pass
+    def goto(self):
+        pass
+    def CanonicalCollection(self):
+        pass
+        #work with ~ in place of .
